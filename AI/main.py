fbc6fa6735306f49ed25b56fa7cce23971d5442a
EXEyad <151939756+EXEyad@users.noreply.github.com>
2024-07-10 21:13:13 +0000

    steps

--- AI/main.py
+++ AI/main.py
@@ -1,36 +1,65 @@
-import enchant.checker as ec
-import pyspellchecker
+from textblob import WordList,Word
+import string as st
 
 NAME = 'Zane'
 VERSION = '0.0.1'
 DEVELOPER ='Eyad Hassan'
+
+
 def ask():
     prompt = input("ask something....\nYou:")
     return prompt
+
+
 def getanswerindex(question:str,questions:list):
-    Index = 0
     for Index in range(len(questions)):
         if questions[Index] == question:
             break
         else:
             continue
     return Index
+
+
 def getanswer(question_index :int,answer:list):
     return answer[question_index]
+
+
+
 def addanswer(questions:list,answers:list,question:str,answer:str):
     questions.append(question)
     answers.append(answer)
     return [questions,answers]
 
 def main():
-    dic = ec.get_default_language()
-    ch =ec.SpellChecker()
     questions =['Hi','Hello','Bye',"Close","Exit","Who are you"]
-    answers =["Hi","Hi","GoodBye","GoodBye","GoodBye",f'I\'m Your Chatbot {NAME} V.{VERSION} I\'d Created By {DEVELOPER}.']
+    answers =["Hi","Hello","GoodBye","GoodBye","GoodBye",f'I\'m Your Chatbot {NAME} V.{VERSION} I\'d Created By {DEVELOPER}.']
     is_r =True
     while is_r:
         prompt = ask()
+        promptstr = ''
+        promptsent = []
+        word = []
+        x = 0
+        if ' ' in prompt.strip():
+            for f in range(len(prompt)):
+                if prompt[f] == ' ':
+                    promptsent.append(promptstr)
+                    print(promptsent)
+                    x+= 1
+                    promptstr =''
+                    
+                else:
+                    prompstr = promptstr.join(prompt[f])
+                    word.append(promptstr)
+                    print(word)
+            ch =WordList(promptsent)
+            promptstr =''
+            print(promptsent)
+        else:
+            ch = Word(prompt)
+        prompt = ch.correct()
         prompt = prompt.capitalize()
+        print(f"searching {prompt}")
         if prompt in questions:
             ind = getanswerindex(prompt,questions)
             print(f"Zain:{getanswer(ind,answers)}")
@@ -38,8 +67,6 @@ def main():
                 break
             else:
                 continue
-        elif ch == True:#uncomplete
-            print(prompt)
         else:
             print('I don\' Understand You!')
 
