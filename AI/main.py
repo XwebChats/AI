e0ef180f544f3f146dabb00a737a26adaafea738
EXEyad <151939756+EXEyad@users.noreply.github.com>
2024-07-10 11:36:01 +0000

    UPT

--- AI/main.py
+++ AI/main.py
@@ -7,7 +7,14 @@ DEVELOPER ='Eyad Hassan'
 def ask():
     prompt = input("ask something....\n")
     return prompt
-
+def getanswerindex(question:str,questions:list):
+    Index = 0
+    for Index in range(len(questions)):
+        if questions[Index] == question:
+            break
+        else:
+            continue
+    return Index
 def getanswer(question_index :int,answer:list):
     return answer[question_index]
 def addanswer(questions:list,answers:list,question:str,answer:str):
@@ -17,11 +24,18 @@ def addanswer(questions:list,answers:list,question:str,answer:str):
 
 def main():
     dic = ec.get_default_language()
-    questions =['Hi','Hello','Bye',"Close","Exit"]
-    answers =["Hi","Hi","GoodBye","GoodBye","GoodBye"]
+    questions =['Hi','Hello','Bye',"Close","Exit","Who are you"]
+    answers =["Hi","Hi","GoodBye","GoodBye","GoodBye",f'I\'m {NAME} V{VERSION} I\'d Created By {DEVELOPER}']
     is_r =True
     while is_r:
         prompt = ask()
+        prompt = prompt.capitalize()
+        ind = getanswerindex(prompt,questions)
+        print(getanswer(ind,answers))
+        if prompt in questions[2:5]:
+            break
+        else:
+            continue
     
 
 
